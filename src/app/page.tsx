a294742ce79bf0bf6692956982a17e6695f1954a
Aakash Anand <aakashanand1995@gmail.com>
2025-10-19 19:33:52 +0530

    final touchups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex justify-center h-screen w-screen items-center font-extrabold">
       <div className="border-2 p-8 m-8">
+        <p>Sign in Required</p>
         <p>
           Go to
           <a className="text-red-500 underline-offset-2" href="/chat">
